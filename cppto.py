2b304f67bdc44979751daee5b079a04a698c86e1
Eric Bitikofer <e.bitikofer@gmail.com>
2017-02-09 12:03:37 -0500

    working cmd arguments

--- cppto.py
+++ cppto.py
@@ -8,31 +8,41 @@
 #You may design your program to quit if the number of arguments is incorrect or
 # prompt the user for the missing file names.
 
-import sys, getopt
+import sys, getopt, re #import argv, exit, parser, and regular expressions
 
-def main(argv):
-    inputfile = ''
-    outputfile = ''
-    try:
-        opts, args = getopt.getopt(argv, "hi:o:", ["ifile=","ofile="])
-    except getopt.GetoptError:
-        print 'test.py -i <inputfile> -o <outputfile>'
-        sys.exit(2)
-    for opt, arg in opts:
-        if opt == '-h':
-            print 'test.py -i <inputfile> -o <outputfile>'
-            sys.exit()
-        elif opt in ("-i", "--ifile"):
-            inputfile = arg
-        elif opt in ("-o", "--ofile"):
-            outputfile = arg
-    print 'Input file is', inputfile
-    print 'Output file is', outputfile
-    infile = open(inputfile, 'r')
-    for line in infile:
-        print line
-if __name__ == "__main__":
-    main(sys.argv[1:])
+tokens = [ 'A-z', 'include', 'using', 'main', 'if', 'else', 'do', 'while', 'for', 'cout', 'cin', '0-9', ' ', ';', '+', '-', '*', '/', '=', '<', '>', '(', ')', '{', '}', '[', ']', '#' ]
+
+def main(argv): #main with input 
+    infile = '' #declared input file string
+    outfile = '' #declared output file string
+    try: #try block for argument check 
+        opts, args = getopt.getopt(argv, "hi:o:", ["ifile=", "ofile="]) #parse funtion for cmdline arguments
+    except getopt.GetoptError: #option error?
+        print('Option not allowed') #print proper format of arguments
+        print('program.py -i <infile> -o <outfile>') #print proper format of arguments
+        sys.exit(2) #exit with error code 2 signifying a command line syntax error
+    if 1 > len(argv) > 4:
+        print('Too many arguments') #print proper format of arguments
+        print('program.py -i <infile> -o <outfile>') #print proper format of arguments
+        sys.exit()
+    for opt, arg in opts: #loop through arguments looking for the input file and output file
+        if opt == '-h': #-h for help prompt
+            print('test.py -i <infile> -o <outfile>') #print proper format of arguments
+            sys.exit() #exit
+        elif opt in ("-i", "--ifile"): #if -i option
+            infile = arg #take in its argument
+        elif opt in ("-o", "--ofile"): #if -o option
+            outfile = arg #take in it argument
+    isfile = open(infile, 'r') 
+    strs = [] #"" for line in isfile]
+    for line in isfile:
+        #print(line)
+        strs.append(line)
+    #print(strs)
+    print(len(argv))
+        #get and parse the line into tokens
+
+
         #if letter A-z
             #if space after, assignment
                  # if x = 1;
@@ -70,6 +80,9 @@ if __name__ == "__main__":
             #if include after, 
                 # if line = #include || using
                     #ignore
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
 
 #Graduate students:
 #Your program should also recognize:
